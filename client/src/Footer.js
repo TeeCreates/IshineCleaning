1a672b761fe338e7634bc4603ef6b3dbda11bdd9
TeeCreates <99111124+TeeCreates@users.noreply.github.com>
2024-03-04 16:05:09 -0500

    footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -11,12 +11,12 @@ const Footer = () => {
         <LogoImage src={footerlogo} alt="" />
         <Li>
           <Linkk href="https://www.instagram.com/ishine_mtl/" target="_blank">
-            <FiInstagram />
+            <FiInstagram size={20} />
           </Linkk>
         </Li>
         <Li>
           <Linkk href="https://www.facebook.com/profile.php?id=100087243824796" target="_blank">
-            <FaFacebookSquare />
+            <FaFacebookSquare size={20}/>
           </Linkk>
         </Li>
       </Ul>
@@ -73,18 +73,19 @@ const Linkk = styled.a`
 
 const Ul = styled.div`
   color: white;
-  font-size: 20px;
+  font-size: 24px;
   display: flex;
   justify-content: center; /* Align items horizontally in the center */
   align-items: center; /* Align items vertically in the center */
 `;
 const UlContact = styled.div`
   color: white;
-  font-size: 20px;
+  font-size: 24px;
   display: flex;
 flex-direction: column;
 /* Align items horizontally in the center */
   align-items: flex-start; /* Align items vertically in the center */
+
 `;
 
 const Li = styled.li`
@@ -92,7 +93,7 @@ const Li = styled.li`
   position: relative;
   top: 5px;
   margin-left: 10px;
-  font-size: 12px;
+  font-size: 14px;
 `;
 
 const StyleNavLink = styled(NavLink)`
@@ -112,5 +113,5 @@ const StyleNavLink = styled(NavLink)`
 `;
 
 const LogoImage = styled.img`
-  height: 35px;
+  height: 40px;
 `;
